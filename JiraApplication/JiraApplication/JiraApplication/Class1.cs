360efe344791bcd5f61f15ef89c20a68f33cc8ea
Alexander Porrello <alexander.porrello@gmail.com>
2018-07-26 07:50:53 -0500

    Jira integration research

---
+++ JiraApplication/JiraApplication/JiraApplication/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraApplication {
+    using Atlassian.Jira;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class JiraTest {
+        public JiraTest() {
+            Jira jiraConn = new Jira("https://issues.tolling.us/", "aporrello", "3Francis");
+            Debug.Print(jiraConn.GetIssue("PANYNJ-760").Assignee);
+
+            //IEnumerable<Atlassian.Jira.Issue> jiraIssues = jiraConn.GetIssuesFromJql(jqlString, 999);
+            //
+            //foreach (var issue in jiraIssues) {
+            //   Debug.WriteLine(issue.Key.Value + " -- " + issue.Summary);
+            //}
+
+        }
+
+        static string PrepareJqlbyDates(string beginDate, string endDate) {
+            string jqlString = "project = PRJ AND status = Closed AND resolved >= " + beginDate + " AND resolved <= " + endDate;
+            return jqlString;
+        }
+    }
+
+}
