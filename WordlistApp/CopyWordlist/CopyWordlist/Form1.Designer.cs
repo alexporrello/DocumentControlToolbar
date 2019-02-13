327465561ed2a15695f642d2d95d8ed5b541835d
Alexander <alexporrello@gmail.com>
2019-02-13 13:22:55 -0600

    Added application to copy wordlists to computer.

---
+++ WordlistApp/CopyWordlist/CopyWordlist/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace CopyWordlist {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.download_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // download_button
+            // 
+            this.download_button.Location = new System.Drawing.Point(197, 178);
+            this.download_button.Name = "download_button";
+            this.download_button.Size = new System.Drawing.Size(120, 23);
+            this.download_button.TabIndex = 0;
+            this.download_button.Text = "Download Wordlists";
+            this.download_button.UseVisualStyleBackColor = true;
+            this.download_button.Click += new System.EventHandler(this.download_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.download_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button download_button;
+    }
+}
+
---
+++ WordlistApp/CopyWordlist/CopyWordlist/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyWordlist {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void download_button_Click(object sender, EventArgs e) {
+            DownloadWordlists();
+        }
+
+        private void DownloadWordlists() {
+            String appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            for (int i = 97; i <= 122; i++) {
+                char character = (char) i;
+                String text = character.ToString();
+                CopyFileToAppData(appData, text);
+            }
+        }
+
+        private void CopyFileToAppData(String appData, String text) {
+            File.Copy(@"Resources/" + text + ".csv", Path.Combine(appData, Path.Combine("DocumentControl", text + ".csv")));
+        }
+    }
+}
