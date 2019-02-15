87dcda0fbe7b8900116279fbc7d3325eb7cda6e2
Alexander <alexporrello@gmail.com>
2019-02-15 07:11:50 -0600

    Copy program throws errors.

--- WordlistApp/CopyWordlist/CopyWordlist/Form1.Designer.cs
+++ WordlistApp/CopyWordlist/CopyWordlist/Form1.Designer.cs
@@ -24,11 +24,12 @@
         /// </summary>
         private void InitializeComponent() {
             this.download_button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // download_button
             // 
-            this.download_button.Location = new System.Drawing.Point(197, 178);
+            this.download_button.Location = new System.Drawing.Point(143, 34);
             this.download_button.Name = "download_button";
             this.download_button.Size = new System.Drawing.Size(120, 23);
             this.download_button.TabIndex = 0;
@@ -36,21 +37,33 @@
             this.download_button.UseVisualStyleBackColor = true;
             this.download_button.Click += new System.EventHandler(this.download_button_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(382, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Click the Download button below to download the wordlists onto your computer.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(403, 71);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.download_button);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Wordlist Downloader";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button download_button;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WordlistApp/CopyWordlist/CopyWordlist/Form1.cs
+++ WordlistApp/CopyWordlist/CopyWordlist/Form1.cs
@@ -22,15 +22,42 @@ namespace CopyWordlist {
         private void DownloadWordlists() {
             String appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
+            Boolean success = true;
+
             for (int i = 97; i <= 122; i++) {
-                char character = (char) i;
+                char character = (char)i;
                 String text = character.ToString();
-                CopyFileToAppData(appData, text);
+                if (CopyFileToAppData(appData, text) == 0) {
+                    System.Windows.Forms.MessageBox.Show("The error log has been copied to your clipboard.", "Download Failed");
+                    success = false;
+                    break;
+                }
+            }
+
+            if (success && CopyFileToAppData(appData, "acronym-duds") == 0) {
+                System.Windows.Forms.MessageBox.Show("The error log has been copied to your clipboard.", "Download Failed");
+            } else {
+                System.Windows.Forms.MessageBox.Show("The copy operation has completed successfully.", "Success!");
             }
         }
 
-        private void CopyFileToAppData(String appData, String text) {
-            File.Copy(@"Resources/" + text + ".csv", Path.Combine(appData, Path.Combine("DocumentControl", text + ".csv")));
+        private int CopyFileToAppData(String appData, String text) {
+            if (!Directory.Exists(Path.Combine(appData, "DocumentControl"))) {
+                Directory.CreateDirectory(Path.Combine(appData, "DocumentControl"));
+            }
+
+            if (File.Exists(Path.Combine(appData, Path.Combine("DocumentControl", text + ".csv")))) {
+                File.Delete(Path.Combine(appData, Path.Combine("DocumentControl", text + ".csv")));
+            }
+
+            try {
+                File.Copy(@"Resources/" + text + ".csv", Path.Combine(appData, Path.Combine("DocumentControl", text + ".csv")));
+            } catch(Exception e) {
+                Clipboard.SetData(DataFormats.Text, e.StackTrace);
+                return 0;
+            }
+
+            return 1;
         }
     }
 }
